ab711e8cc20c5738d722b57995c0702d9c1561a8
Harry Seong <harryseong@gmail.com>
2022-10-07 10:22:29 +0800

    Added Home and About components

---
+++ src/components/about/About.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+import styles from './About.module.scss';
+
+export function About() {
+
+    return (
+        <div>
+            <h1>About this site.</h1>
+        </div>
+    );
+}
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+import styles from './Home.module.scss';
+
+export function Home() {
+
+    return (
+        <div>
+            <h1>Welcome Home</h1>
+        </div>
+    );
+}
