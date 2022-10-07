0c2a77cd563170a55b0b95ecca29cd55b65a5d27
Harry Seong <harryseong@gmail.com>
2022-10-07 11:25:07 +0800

    Added app routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,56 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Layout } from '../components/layout/Layout';
 import { Counter } from '../components/counter/Counter';
+import { Home } from '../components/home/Home';
+import { About } from '../components/about/About'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p> 
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/counter" element={<Counter />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+export function Layout() {
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/counter">Counter</Link></li>
+                </ul>
+            </nav>
+
+            <Outlet/>
+        </div>
+    )
+}
